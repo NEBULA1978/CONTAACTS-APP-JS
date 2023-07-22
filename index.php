9e7b5695775cd9f4bee1159c8769538a83a39089
NEBULA1978 <rpascualey@gmail.com>
2023-07-22 19:51:25 +0200

    Añadiendo pagina inicio menu variado sin login

--- index.php
+++ index.php
@@ -1,13 +1,76 @@
 <?php require "partials/header.php" ?>
+<link rel="stylesheet" href="css/estilos.css" />
 
 <div class="welcome d-flex align-items-center justify-content-center">
 
-    
+
 
     <div class="text-center">
         <h1>Store Your Contacts Now</h1>
         <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
     </div>
 </div>
+<header>
+    <nav class="contenedor-flex-1">
+        <a href="index.html">
+            <img src="img/logo.png" alt="logo" width="250" height="50" />
+        </a>
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="nosotros.html">Nosotros Ejercicios Javascript</a></li>
+            <li><a href="contacto.html">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
+
+<!-- Banner -->
+<section id="banner">
+    <div class="ancho-contenido-banner">
+        <h1>Somos Expertos en Desarrollo WEB</h1>
+        <p>
+            Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Facilis
+            fuga hic modi. Laudantium quod a eum, soluta nam aliquid ab quidem
+            totam voluptatibus dolorum provident corrupti ut architecto assumenda
+            dolor.
+        </p>
+    </div>
+</section>
+
+<!-- Cuota -->
+<section class="cuota">
+    <span>¿Quieres recibir una cuota hoy mismo?</span>
+    <a href="contacto.html">Contáctanos</a>
+</section>
+
+<!-- Servicios -->
+<section class="contenedor-flex-2">
+    <div>
+        <img src="img/computadora.png" alt="icono computadora" />
+        <h2>Páginas WEB</h2>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            eaque distinctio ducimus nemo reprehenderit praesentium quia autem
+            sint natus. Laborum!
+        </p>
+    </div>
+    <div>
+        <img src="img/celular.png" alt="icono celular" />
+        <h2>Aplicaciones</h2>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+            explicabo sint nulla, expedita animi totam saepe doloribus. Quasi,
+            atque, error.
+        </p>
+    </div>
+    <div>
+        <img src="img/cart.png" alt="icono cart" />
+        <h2>Tiendas Virtuales</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempora
+            ipsum explicabo nulla nam quibusdam beatae nemo asperiores illo
+            assumenda?
+        </p>
+    </div>
+</section>
 
 <?php require "partials/footer.php" ?>
