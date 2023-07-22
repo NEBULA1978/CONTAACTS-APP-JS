1111739057fae9addd160d99991626ac7246cab7
NEBULA1978 <rpascualey@gmail.com>
2023-07-22 20:19:17 +0200

    Añado menu para ususarios no registrado que vean ejercicio javascript arreglando carpetas y css arreglada rutas htmls

--- index.php
+++ index.php
@@ -10,67 +10,67 @@
         <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
     </div>
 </div>
-   <header>
-        <nav class="contenedor-flex-1">
-            <a href="index.html">
-               
-            </a>
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="nosotros.html">Nosotros Ejercicios Javascript</a></li>
-                <li><a href="contacto.html">Contacto</a></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav class="contenedor-flex-1">
+        <a href="./htmls/index.html">
 
-    <!-- Banner -->
-    <section id="banner">
-        <div class="ancho-contenido-banner">
-            <h1>Somos Expertos en Desarrollo WEB</h1>
-            <p>
-                Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Facilis
-                fuga hic modi. Laudantium quod a eum, soluta nam aliquid ab quidem
-                totam voluptatibus dolorum provident corrupti ut architecto assumenda
-                dolor.
-            </p>
-        </div>
-    </section>
+        </a>
+        <ul>
+            <li><a href="./htmls/index.html">Inicio</a></li>
+            <li><a href="./htmls/nosotros.html">Nosotros Ejercicios Javascript</a></li>
+            <li><a href="./htmls/contacto.html">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
 
-    <!-- Cuota -->
-    <section class="cuota">
-        <span>¿Quieres recibir una cuota hoy mismo?</span>
-        <a href="contacto.html">Contáctanos</a>
-    </section>
+<!-- Banner -->
+<section id="banner">
+    <div class="ancho-contenido-banner">
+        <h1>Somos Expertos en Desarrollo WEB</h1>
+        <p>
+            Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Facilis
+            fuga hic modi. Laudantium quod a eum, soluta nam aliquid ab quidem
+            totam voluptatibus dolorum provident corrupti ut architecto assumenda
+            dolor.
+        </p>
+    </div>
+</section>
 
-    <!-- Servicios -->
-    <section class="contenedor-flex-2">
-        <div>
-            <img src="img/computadora.png" alt="icono computadora" />
-            <h2>Páginas WEB</h2>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-                eaque distinctio ducimus nemo reprehenderit praesentium quia autem
-                sint natus. Laborum!
-            </p>
-        </div>
-        <div>
-            <img src="img/celular.png" alt="icono celular" />
-            <h2>Aplicaciones</h2>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
-                explicabo sint nulla, expedita animi totam saepe doloribus. Quasi,
-                atque, error.
-            </p>
-        </div>
-        <div>
-            <img src="img/cart.png" alt="icono cart" />
-            <h2>Tiendas Virtuales</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempora
-                ipsum explicabo nulla nam quibusdam beatae nemo asperiores illo
-                assumenda?
-            </p>
-        </div>
-    </section>
+<!-- Cuota -->
+<section class="cuota">
+    <span>¿Quieres recibir una cuota hoy mismo?</span>
+    <a href="contacto.html">Contáctanos</a>
+</section>
+
+<!-- Servicios -->
+<section class="contenedor-flex-2">
+    <div>
+        <img src="img/computadora.png" alt="icono computadora" />
+        <h2>Páginas WEB</h2>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            eaque distinctio ducimus nemo reprehenderit praesentium quia autem
+            sint natus. Laborum!
+        </p>
+    </div>
+    <div>
+        <img src="img/celular.png" alt="icono celular" />
+        <h2>Aplicaciones</h2>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+            explicabo sint nulla, expedita animi totam saepe doloribus. Quasi,
+            atque, error.
+        </p>
+    </div>
+    <div>
+        <img src="img/cart.png" alt="icono cart" />
+        <h2>Tiendas Virtuales</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempora
+            ipsum explicabo nulla nam quibusdam beatae nemo asperiores illo
+            assumenda?
+        </p>
+    </div>
+</section>
 
 <?php require "partials/footer.php" ?>
